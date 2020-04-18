409bf668423faca6ad50a275a9703700f8601c2d
Slav Lukyanov <g00339839@gmit.ie>
2020-04-18 04:21:08 +0000

    Update,delete work well

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,5 +1,5 @@
 class CitiesController < ApplicationController
-  before_action :set_city, only: [:show, :edit, :update, :destroy]
+  before_action :set_city, only: [:show, :index, :edit, :update, :destroy]
 
   # GET /cities
   # GET /cities.json
@@ -13,6 +13,7 @@ class CitiesController < ApplicationController
   # GET /cities/1
   # GET /cities/1.json
   def show
+     @city = City.find(params[:id])
   end
 
   # GET /cities/new
@@ -22,52 +23,47 @@ class CitiesController < ApplicationController
 
   # GET /cities/1/edit
   def edit
+     @city = City.find(params[:id])
   end
 
   # POST /cities
   # POST /cities.json
   def create
-    @city = City.new(city_params)
+   
 
-    respond_to do |format|
-      if @city.save
-        format.html { redirect_to @city, notice: 'City was successfully created.' }
-        format.json { render :show, status: :created, location: @city }
-      else
-        format.html { render :new }
-        format.json { render json: @city.errors, status: :unprocessable_entity }
-      end
-    end
+    @city = City.find params[:id]
+    @city.update_attributes!(city_params)
+    flash[:notice] = "#{@city.city_name} was successfully updated."
+    redirect_to cities_path(@city)
+
+  
   end
 
   # PATCH/PUT /cities/1
   # PATCH/PUT /cities/1.json
   def update
-    respond_to do |format|
-      if @city.update(city_params)
-        format.html { redirect_to @city, notice: 'City was successfully updated.' }
-        format.json { render :show, status: :ok, location: @city }
-      else
-        format.html { render :edit }
-        format.json { render json: @city.errors, status: :unprocessable_entity }
-      end
-    end
+  
+    @city = City.find params[:id]
+    @city.update_attributes!(city_params)
+    flash[:notice] = "#{@city.city_name} was successfully updated."
+    redirect_to cities_path(@city)
   end
 
   # DELETE /cities/1
   # DELETE /cities/1.json
   def destroy
+   
+    @city = City.find params[:id]
     @city.destroy
-    respond_to do |format|
-      format.html { redirect_to cities_url, notice: 'City was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:notice] = "Movie '#{@city.city_name} ' deleted."
+    redirect_to cities_path(@city)
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_city
-      @city = City.find(params[:id])
+     # @city = City.find(params[:id])
+      
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
