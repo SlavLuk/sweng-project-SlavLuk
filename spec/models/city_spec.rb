0d71a0ade35e2933e758dea48a0de1e07a393337
Slav Lukyanov <g00339839@gmit.ie>
2020-05-23 23:45:31 +0000

    Passed spec search city

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,41 @@
+require 'spec_helper'
 require 'rails_helper'
 
-RSpec.describe City, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe City do
+
+ before do 
+
+      @fake_countries = [Country.create!(:country_name=>'Ireland'.upcase)]
+
+      @fake_cities= [City.create!(:city_name => 'Galway', :mayor => 'Pat Larkin', 
+                       :population => 75000, :isCostal => true, :country =>@fake_countries[0]),
+                     City.create!(:city_name => 'Cork', :mayor => 'Paddy Smith', 
+                       :population => 350000, :isCostal => true, :country =>@fake_countries[0])]
+        end
+
+  describe "#same country" do
+    it "should find cities in the same country" do
+    
+        results = City.search_city(@fake_cities[0].country_id) 
+         
+        expect(results).to eq( @fake_cities)
+   
+    
+        
+        end
+
+    end
 end
+
+
+
+
+
+
+
+
+
+
+# RSpec.describe City, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
